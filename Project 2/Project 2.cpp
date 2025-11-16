1d7b442fb9c9948312da0d8a3eae50df187605f6
osama mohamoud <wwwhdosama@gmail.com>
2025-11-16 20:58:52 +0200

    Auto commit: 2025-11-16 20:58:50

---
+++ Project 2/Project 2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "clsString.h"
+using namespace std;
+
+int main() {
+
+    clsString String;
+    String.SetValue("osama mohamoud");
+    cout << String.CountWord() << endl;
+    // String.PrintFirstLetter();
+    // clsString::PrintFirstLetter("kaka lala tka clk");
+
+    // cout<<String.UppercaseFirstLetterOfEachWord()<<endl;
+    // cout<<clsString::UppercaseFirstLetterOfEachWord("song is good!")<<endl;
+    //
+    // cout<<clsString::LowercaseFirstLetterOfEachWord("Life Is About:")<<endl;
+    // cout<<String.LowercaseFirstLetterOfEachWord()<<endl;
+
+
+
+
+}
---
+++ Project 2/clsString.h
@@ -0,0 +1,134 @@
+#pragma once
+#include<iostream>
+#include<cctype>
+#include <string>
+#include<vector>
+using namespace std;
+
+class clsString {
+
+private:
+    string _value ;
+public:
+    //no-args constructor 
+    clsString() {
+        this->_value = "";
+    }
+
+    //parametrized constructor 
+    clsString(string value) {
+        this->_value = value;
+    }
+
+    //property set
+    void SetValue(string value) {
+        this->_value = value;
+    }
+
+    //property get
+    string GetValue(){return this->_value;}
+
+// count word//
+    static short CountWord(string value, string delmi=" ") {
+        vector<string> word = SplitString(value); // cut the string into words
+        return word.size();
+    }
+
+    short CountWord() {
+        return CountWord(this->_value, " ");
+    }
+
+    // The first letter of each word//
+    static void PrintFirstLetter(string value , string delmi=" ") {
+        vector<string> word = SplitString(value); // cut the string into words
+        for (string &w :word) {
+            //temp var
+            string TempWord = w;
+           cout<<TempWord.at(0)<<endl;
+        }
+    }
+    void PrintFirstLetter() {
+        PrintFirstLetter(this->_value, " ");
+    }
+
+    // uppercase the first letter//
+private:
+    static string UppercaseTheFirstLetter(string value) {
+        string res="" ;
+        res += toupper(value.at(0));
+        for (int i=1; i<value.length(); i++) {
+
+            res+=value[i];
+
+        }
+        return res;
+    }
+    static string LowercaseTheFirstLetter(string value) {
+        string res="" ;
+        res += tolower(value.at(0));
+        for (int i=1; i<value.length(); i++) {
+
+            res+=value[i];
+
+        }
+        return res;
+    }
+    // cut the full sentence into vector of words
+   static vector<string>SplitString(string value, string delmi=" ") {
+        short pos ;
+        vector<string>FinalRes;
+        string word ;
+        while ((pos=value.find(delmi))!=string::npos) {
+            word= value.substr(0,pos); // get the word
+            if (word!="") FinalRes.push_back(word);// uppercase the current word
+
+            value.erase(0,pos+delmi.length());
+        }
+        if (value!=" ") FinalRes.push_back(value);
+        return FinalRes;
+    }
+
+
+
+
+public:
+
+    static string UppercaseFirstLetterOfEachWord(string value, string delmi=" ") {
+
+     string finalRes="";
+        vector<string> word = SplitString(value); // cut the string into words
+        for (string &w :word) {
+              //temp var
+            string TempWord = w;
+            TempWord=UppercaseTheFirstLetter(TempWord);
+            finalRes+=TempWord+delmi;
+        }
+          return finalRes;
+    }
+
+    string UppercaseFirstLetterOfEachWord() {
+        return UppercaseFirstLetterOfEachWord(this->_value);
+    }
+
+    // lower the first letter  for each word//
+    static string LowercaseFirstLetterOfEachWord(string value, string delmi=" ") {
+        string finalRes="";
+        vector<string> word = SplitString(value); // cut the string into words
+        for (string &w :word) {
+            //temp var
+            string TempWord = w;
+            TempWord=LowercaseTheFirstLetter(TempWord);
+            finalRes+=TempWord+delmi;
+        }
+        return finalRes;
+    }
+ string LowercaseFirstLetterOfEachWord() {
+        return LowercaseFirstLetterOfEachWord(this->_value);
+    }
+
+
+
+
+};
+
+
